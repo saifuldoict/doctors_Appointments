948510bd642bfbfd0d2980d12704a8c9e0c81da3
Md. Saiful Islam <saiful.doict@gmail.com>
2025-04-07 22:03:40 +0600

    assets

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,16 @@
+import add_doctor_icon from './add_doctor_icon.png';
+
+import appointments_icon from './appointments_icon.svg';
+import dashboard_icon from './dashboard_icon.png';
+import doctor_icon from './doctor_icon.svg';
+import list_icon from './list_icon.svg';
+import patients_icon from './patients_icon.svg';
+
+export const assets = {
+        add_doctor_icon,
+        appointments_icon,
+        dashboard_icon,
+        doctor_icon,
+        list_icon,
+        patients_icon
+}
